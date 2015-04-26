0f5a963a0ff84770558361e9ae649d99b7a43fa6
sietemonos <sietemonos@gmail.com>
2015-04-26 16:29:06 -0500

    R-Analysis Samsung Data Set
    The run_analysis.R script creates a tidy data with mean values from
    Samsung Data Set.

---
+++ run_analysis.R
@@ -0,0 +1,109 @@
+## Creates a tidy data set from the Samsung data set
+##
+## WARNNING:
+## Only works if the "UCI HAR Dataset" directory is in the working directory.
+## You can download and unzip the Samsung data set from:
+## https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+## 
+## Inputs:
+##   
+
+library(plyr)
+
+dir_path <- dirname(sys.frame(1)$ofile)
+DIR_DATA <- "UCI HAR Dataset"
+FILE_URL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+
+check_script <- function(x, y){
+  # Checks working directory matchs with the script and data directory
+  #
+  # Args: 
+  #   x: Directory path of the source file.
+  #   y: Directory name of the Samsung data set
+  #
+  # Returns:
+  #   TRUE or FALSE
+  
+  # a. Check if Samsung data set is in the project directory
+  if (file_test("-d", dir_path, DIR_DATA)) {
+    
+    # b. Check if project directory is the working directory
+    if (dir_path != getwd()) {
+      setwd(dir_path)  # Set the working directory with dir_path
+      
+    }
+    return (TRUE)
+  } else {
+    msg <- paste("The Samsung data set is not in the project directory",
+                 "Please download the data set from:",
+                 FILE_URL, sep = "\n")
+    return (msg)
+  }
+}
+
+extract_mean_and_sd <- function(x){
+  # Extracts only the mean and standard deviation for each measurement.
+  filtered_data <<- x[, c(1, 2, grep("mean\\(\\)|std\\(\\)", names(x)))]
+}
+
+merge_data <- function(){
+  # Merges the training and the test sets to create one data set.
+  
+  # Gets paths for the data main directory and subdirectories test and train.
+  data_path <- list.dirs()[which(grepl(DIR_DATA, list.dirs()))[1]]
+  
+  if (!exists("data_sets")) {
+    
+    d <- list.dirs(data_path, recursive = FALSE) # Test and train paths
+    
+    # Reads txt files in test and train directories.
+    ft <- list.files(d, pattern = "^.*.txt$", full.names = TRUE)
+    data_sets <<- lapply(ft, read.table, header = FALSE)
+  }
+  
+  if (!exists("vn")) {
+    # Reads the variable names in features.txt
+    vn <<- read.table(file.path(data_path, "features.txt"), header = FALSE)
+  }
+  
+  # Merges with cbind and rbind because data sets there are no common keys
+  test_data <- as.data.frame(cbind(data_sets[[3]], data_sets[[1]],
+                                   data_sets[[2]]))
+  train_data <- as.data.frame(cbind(data_sets[[6]], data_sets[[4]], 
+                                    data_sets[[5]]))
+  total_data <<- rbind(test_data, train_data)
+  
+  # Sets the variable names to total_data
+  names(total_data) <<- c(list("Activity", "Subject"),
+                         c(sapply(vn[[2]], paste, sep = ",")))
+
+  # Extracts only the mean and standard deviation for each measurement.
+  filtered_data <<- total_data[, c(1, 2, grep("mean\\(\\)|std\\(\\)", 
+                                              names(total_data)))]
+  
+  al <<- read.table(file.path(data_path, "activity_labels.txt"), header = FALSE)[, 2]
+  
+  filtered_data[, 1] <<- sub("1", al[1], filtered_data[, 1])
+  #filtered_data[, 1] <- sub("2", al[2], filtered_data[, 1])
+
+  
+  names(filtered_data) <<- gsub("^t", "time", names(filtered_data))
+  names(filtered_data) <<- gsub("^f", "frequency", names(filtered_data))
+  names(filtered_data) <<- gsub("Acc", "Accelerometer", names(filtered_data))
+  names(filtered_data) <<- gsub("Gyro", "Gyroscope", names(filtered_data))
+  names(filtered_data) <<- gsub("Mag", "Magnitude", names(filtered_data))
+  names(filtered_data) <<- gsub("BodyBody", "Body", names(filtered_data))
+
+  # quitar paréntesis
+}
+
+
+
+if (check_script(data_path, DIR_DATA)) {
+  merge_data()
+} else {
+  stop(msg)  # stops execution and sends an error message
+}
+
+#extract_mean_and_sd(total_data)
+#change_activity_labels()
